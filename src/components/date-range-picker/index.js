36b01911809240ab7e2e9d91a7ba71b5690bb475
Tyler Hawkins <thawkin3@byu.net>
2023-04-07 16:16:49 -0600

    feat: adds focus trap for improved keyboard and screen reader experience

--- src/components/date-range-picker/index.js
+++ src/components/date-range-picker/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import FocusTrap from 'focus-trap-react';
 import { days, months } from './constants';
 import './index.css';
 
@@ -7,6 +8,8 @@ export const DateRangePicker = () => {
   const [endDate, setEndDate] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
   const [currentMonthIndex, setCurrentMonthIndex] = useState(3);
+
+  const dateRangePickerTriggerButtonRef = useRef();
 
   const toggleDatePicker = () => {
     setIsOpen((isOpen) => !isOpen);
@@ -36,11 +39,23 @@ export const DateRangePicker = () => {
     toggleDatePicker();
   };
 
+  const handleEscapeKey = (e) => {
+    if (e.key === 'Escape') {
+      toggleDatePicker();
+    }
+  };
+
+  const handleOutsideClick = () => {
+    console.log('handleOutsideClick', dateRangePickerTriggerButtonRef.current);
+    toggleDatePicker();
+    dateRangePickerTriggerButtonRef.current?.focus();
+  };
+
   return (
     <div data-testid="date-range-picker" className="dateRangePicker">
       <p>
         Start date:{' '}
-        {startDate?.toLocaleDateString(undefined, {
+        {startDate?.toLocaleDateString('en-US', {
           weekday: 'short',
           year: 'numeric',
           month: 'short',
@@ -49,7 +64,7 @@ export const DateRangePicker = () => {
       </p>
       <p>
         End date:{' '}
-        {endDate?.toLocaleDateString(undefined, {
+        {endDate?.toLocaleDateString('en-US', {
           weekday: 'short',
           year: 'numeric',
           month: 'short',
@@ -61,87 +76,104 @@ export const DateRangePicker = () => {
         onClick={toggleDatePicker}
         data-testid="date-range-picker-trigger-button"
         className="dateRangePickerTriggerButton"
+        ref={dateRangePickerTriggerButtonRef}
       >
         Select date range
       </button>
       {isOpen && (
-        <div
-          data-testid="date-range-picker-calendar"
-          className="dateRangePickerCalendar"
-        >
-          <div
-            data-testid="date-range-picker-calendar-controls"
-            className="dateRangePickerCalendarControls"
-          >
-            <span>{months[currentMonthIndex]} 2023</span>
-            <span
-              data-testid="date-range-picker-next-previous-buttons"
-              className="dateRangePickerNextPreviousButtons"
+        <FocusTrap>
+          <div>
+            <div
+              data-testid="date-range-picker-background-underlay"
+              className="dateRangePickerBackgroundUnderlay"
+              onClick={handleOutsideClick}
+            />
+            <div
+              data-testid="date-range-picker-calendar"
+              className="dateRangePickerCalendar"
+              onKeyDown={handleEscapeKey}
             >
-              <button
-                type="button"
-                onClick={goToPreviousMonth}
-                disabled={currentMonthIndex === 0}
-                aria-label="Previous Month"
+              <div
+                data-testid="date-range-picker-calendar-controls"
+                className="dateRangePickerCalendarControls"
               >
-                &lt;
-              </button>
-              <button
-                type="button"
-                onClick={goToNextMonth}
-                disabled={currentMonthIndex === 11}
-                aria-label="Next Month"
-              >
-                &gt;
-              </button>
-            </span>
-          </div>
-          <div
-            data-testid="date-range-picker-calendar-days"
-            className="dateRangePickerCalendarDays"
-          >
-            {days[currentMonthIndex].map((day, index) => {
-              const indexOfFirstDayOfMonth = days[currentMonthIndex].indexOf(1);
-              const isPreviousMonth = index < indexOfFirstDayOfMonth;
-              const isNextMonth =
-                day < 15 && index > days[currentMonthIndex].indexOf(day);
-
-              const monthIndexForThisDay = isPreviousMonth
-                ? currentMonthIndex - 1
-                : isNextMonth
-                ? currentMonthIndex + 1
-                : currentMonthIndex;
-
-              const fullDateForDay = new Date(2023, monthIndexForThisDay, day);
-              const isSelected =
-                startDate?.toDateString() === fullDateForDay.toDateString() ||
-                endDate?.toDateString() === fullDateForDay.toDateString();
-
-              const isInsideSelectedRange =
-                startDate &&
-                endDate &&
-                fullDateForDay > startDate &&
-                fullDateForDay < endDate;
-
-              return (
-                <button
-                  key={`${monthIndexForThisDay}-${day}-2023-${index}`}
-                  data-testid="date-range-picker-calendar-day"
-                  className={`dateRangePickerCalendarDay${
-                    isSelected ? ' selected' : ''
-                  }${isInsideSelectedRange ? ' dayInRange' : ''}`}
-                  type="button"
-                  onClick={handleDayClick}
-                  data-day={day}
-                  data-month={monthIndexForThisDay + 1}
-                  data-year={2023}
+                <span>{months[currentMonthIndex]} 2023</span>
+                <span
+                  data-testid="date-range-picker-next-previous-buttons"
+                  className="dateRangePickerNextPreviousButtons"
                 >
-                  {day}
-                </button>
-              );
-            })}
+                  <button
+                    type="button"
+                    onClick={goToPreviousMonth}
+                    disabled={currentMonthIndex === 0}
+                    aria-label="Previous Month"
+                  >
+                    &lt;
+                  </button>
+                  <button
+                    type="button"
+                    onClick={goToNextMonth}
+                    disabled={currentMonthIndex === 11}
+                    aria-label="Next Month"
+                  >
+                    &gt;
+                  </button>
+                </span>
+              </div>
+              <div
+                data-testid="date-range-picker-calendar-days"
+                className="dateRangePickerCalendarDays"
+              >
+                {days[currentMonthIndex].map((day, index) => {
+                  const indexOfFirstDayOfMonth =
+                    days[currentMonthIndex].indexOf(1);
+                  const isPreviousMonth = index < indexOfFirstDayOfMonth;
+                  const isNextMonth =
+                    day < 15 && index > days[currentMonthIndex].indexOf(day);
+
+                  const monthIndexForThisDay = isPreviousMonth
+                    ? currentMonthIndex - 1
+                    : isNextMonth
+                    ? currentMonthIndex + 1
+                    : currentMonthIndex;
+
+                  const fullDateForDay = new Date(
+                    2023,
+                    monthIndexForThisDay,
+                    day
+                  );
+                  const isSelected =
+                    startDate?.toDateString() ===
+                      fullDateForDay.toDateString() ||
+                    endDate?.toDateString() === fullDateForDay.toDateString();
+
+                  const isInsideSelectedRange =
+                    startDate &&
+                    endDate &&
+                    fullDateForDay > startDate &&
+                    fullDateForDay < endDate;
+
+                  return (
+                    <button
+                      key={`${monthIndexForThisDay}-${day}-2023-${index}`}
+                      data-testid="date-range-picker-calendar-day"
+                      className={`dateRangePickerCalendarDay${
+                        isSelected ? ' selected' : ''
+                      }${isInsideSelectedRange ? ' dayInRange' : ''}`}
+                      type="button"
+                      onClick={handleDayClick}
+                      data-day={day}
+                      data-month={monthIndexForThisDay + 1}
+                      data-year={2023}
+                    >
+                      {day}
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
           </div>
-        </div>
+        </FocusTrap>
       )}
     </div>
   );
